78c57d3d47d0dc251465b796538620f0536eb497
pengguoqi <2472324889@qq.com>
2024-04-18 11:00:48 +0800

    add

---
+++ include/g2o/config.h
@@ -0,0 +1,28 @@
+#ifndef G2O_CONFIG_H
+#define G2O_CONFIG_H
+
+#define G2O_HAVE_OPENGL 1
+#define G2O_OPENGL_FOUND 1
+/* #undef G2O_OPENMP */
+#define G2O_SHARED_LIBS 1
+/* #undef G2O_LGPL_SHARED_LIBS */
+
+// available sparse matrix libraries
+/* #undef G2O_HAVE_CHOLMOD */
+/* #undef G2O_HAVE_CSPARSE */
+
+// logging framework available
+/* #undef G2O_HAVE_LOGGING */
+
+/* #undef G2O_NO_IMPLICIT_OWNERSHIP_OF_OBJECTS */
+
+#ifdef G2O_NO_IMPLICIT_OWNERSHIP_OF_OBJECTS
+#define G2O_DELETE_IMPLICITLY_OWNED_OBJECTS 0
+#else
+#define G2O_DELETE_IMPLICITLY_OWNED_OBJECTS 1
+#endif
+
+#define G2O_CXX_COMPILER "GNU /usr/bin/c++"
+#define G2O_SRC_DIR "/home/pgq/work/github/pp/g2o"
+
+#endif
--- zip_lib.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cp -r ./build/lib ./
-mkdir include
-find ./g2o/ -type f -name "*.h" -exec cp --parents \{\} ./include \;
-cp ./build/g2o/config.h ./include/g2o
