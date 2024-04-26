21badfe06805f46d942f34a0a37d8a388fd9ec23
pengguoqi <2472324889@qq.com>
2024-04-26 14:16:24 +0800

    update

--- zip_lib.sh
+++ zip_lib.sh
@@ -2,4 +2,5 @@
 cp -r ./build/lib ./
 mkdir include
 find ./g2o/ -type f -name "*.h" -exec cp --parents \{\} ./include \;
+find ./g2o/ -type f -name "*.hpp" -exec cp --parents \{\} ./include \;
 cp ./build/g2o/config.h ./include/g2o
